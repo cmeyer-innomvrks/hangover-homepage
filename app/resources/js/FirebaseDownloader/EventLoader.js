4cb5cc3bce7f27eb24630e8efac60f1e8253f12b
mec60883 <41949341+mec60883@users.noreply.github.com>
2020-03-02 10:12:12 +0100

    Added First JS File

---
+++ app/resources/js/FirebaseDownloader/EventLoader.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import firestore from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id",
+};
+
+firebase.initializeApp(firebaseConfig);
