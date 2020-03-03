0cb9663d399794592b8a1e78435d55d8f3692620
mec60883 <41949341+mec60883@users.noreply.github.com>
2020-03-03 10:53:51 +0100

    small fixes

--- app/resources/js/UI/EventList.js
+++ app/resources/js/UI/EventList.js
@@ -9,27 +9,23 @@ class EventList extends View {
 
     setElement(element) {
         super.setElement(element);
-        element.addEventListener("click", this.onClick.bind(this));
     }
 
     onClick(event) {
-        let target = event.target;
-        console.log(target);
+        console.log("Klick!");
     }
 
     displayEvents(eventItems) {
-        console.log(eventItems);
-        for (let i = 0; i < 2; i++) {
-            console.log(eventItems);
-
+        for (let i = 0; i < eventItems.length; i++) {
             let template = document.querySelector(".event-template").innerHTML.trim(),
                 item = document.createElement("div");
             item.innerHTML = template;
             item.querySelector(".card-img-top").src = eventItems[i].image;
             item.querySelector(".headline").textContent = eventItems[i].headline;
-            item.querySelector(".shortDescription").textContent = eventItems[i].info;
+            item.querySelector(".shortDescription").textContent = eventItems[i].info.substring(0, 150) + "...";
             item.querySelector(".event-location").textContent = "@" + eventItems[i].locationid;
-            item.querySelector(".event-time").textContent = eventItems[i].time;
+            item.querySelector(".event-time").textContent = eventItems[i].time + "Uhr";
+            item.addEventListener("click", this.onClick.bind(this));
             this.element.appendChild(item);
         }
     }
