743b3579330344f8e58c107d317076dd4b6bdffe
swstudentrgb <54363770+swstudentrgb@users.noreply.github.com>
2020-03-10 16:30:53 +0100

    Map v1 finished

--- app/resources/js/map.js
+++ app/resources/js/map.js
@@ -1,55 +1,41 @@
-/* eslint-env browser */
-
-import platform from "./Maps/mapk.js";
-
-function init() {
-    myMap();
-}
-
-function myMap() {
-    // Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-var map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.vector.normal.map,
-    {
-      zoom: 14,
-      center: { lat: 49.014335, lng: 12.094758 }
-    });
-
-    var animatedSvg ='<svg width="24" height="24" ' +
-    'xmlns="http://www.w3.org/2000/svg">' +
-    '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
-    'height="22" /><text x="12" y="18" font-size="12pt" ' +
-    'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
-    'fill="white">H</text></svg>';
-
-    var icon = new H.map.DomIcon(animatedSvg),
-    coords = { lat: 49.014335, lng: 12.094758},
-    marker = new H.map.DomMarker(coords, {icon: icon});
-
-    map.addObject(marker);
-// Create the default UI:
-var ui = H.ui.UI.createDefault(map, defaultLayers, 'de-DE');
-var mapSettings = ui.getControl('mapsettings');
-var zoom = ui.getControl('zoom');
-var scalebar = ui.getControl('scalebar');
-
-mapSettings.setAlignment('top-left');
-zoom.setAlignment('top-left');
-scalebar.setAlignment('top-left');
-
-// Create an info bubble object at a specific geographic location:
-var bubble = new H.ui.InfoBubble({ lat: 49.014335, lng: 12.094758 }, {
-    content: '<b>Beats Club</b>'
- });
-
-// Add info bubble to the UI:
-ui.addBubble(bubble);
-}
-
-
-init();
-
+import platform from "../js/Maps/mapk.js";
+/**
+ * Moves the map to display over Berlin
+ *
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
+function moveMapToRegensburg(map){
+    map.setCenter({lat:49.014335, lng:12.094758});
+    map.setZoom(14);
+  }
+  
+  /**
+   * Boilerplate map initialization code starts below:
+   */
+  
+  //Step 1: initialize communication with the platform
+  // In your own code, replace variable window.apikey with your own apikey
+  var defaultLayers = platform.createDefaultLayers();
+  
+  //Step 2: initialize a map - this map is centered over Europe
+  var map = new H.Map(document.getElementById('mapContainer'),
+    defaultLayers.vector.normal.map,{
+    center: {lat:50, lng:5},
+    zoom: 4,
+    pixelRatio: window.devicePixelRatio || 1
+  });
+  // add a resize listener to make sure that the map occupies the whole container
+  window.addEventListener('resize', () => map.getViewPort().resize());
+  
+  //Step 3: make the map interactive
+  // MapEvents enables the event system
+  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+  
+  // Create the default UI components
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+  
+  // Now use the map as required...
+  window.onload = function () {
+    moveMapToRegensburg(map);
+  } 
